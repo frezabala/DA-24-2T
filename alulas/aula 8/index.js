c18a49c41d3f064352361359c3380f9257fe3ebb
ingrib30 <ingridbernardes@live.com>
2024-08-26 17:25:04 -0300

    aululas

--- alulas/aula 8/index.js
+++ alulas/aula 8/index.js
@@ -32,6 +32,48 @@ const frase = 'hoje comi cenora adoro cenora'
 const novaFrase = frase.includes("cenora" , "batata")
 console.log(novaFrase)
 
+*//*
+ const frutas = ["abacate", "banana", "tomate", "milho", "uva", "morango"]
+ console.log(frutas)
+
+ const fruta = fruta[2]
+ console.log(fruta)
 */
+/*
 
+const racas =["Pastor-alemão", "Boiadeiro de Berna", 
+"Samoieda", "Cão Esquimó Americano", "American Bully"]
+const cachoro = Number(prompt("insiera um numero de 0 a 4"))
+console.log(racas[cachoro])
+*/
+/*
+const pokemons = ["Ivysaur", "Charizard", "Genesect", "Bulbasaur"]
+console.log(pokemons.length)
+*//*
+const serieBoas = ["vikings", "the big bang theory"]
+console.log(serieBoas.includes("vikings"))
+console.log(serieBoas.includes("game oj thrones"))*/
+/*
+const numero = [1,2,3]
 
+numero.push(4)
+console.log(numero)
+numero.push(5,6,7,8)
+console.log(numero)*/
+/*
+const peixe = ["palhaso", "mandarin", "esturjao"]
+
+peixe.pop()
+console.log(peixe)*/
+/*
+const letra =["A","B","C","D","E","F","G","H"]
+letra.splice(2,1)
+console.log(letra)
+letra.splice(5,2)
+console.log(letra)*/
+/*
+const numero = [1,2,3,4,5]
+console.log(numero.length)
+numero.push(7)
+numero.splice(3,2)
+numero*/
---
+++ alulas/aula 9/index.js
@@ -0,0 +1,57 @@
+
+/*se encontra declarar um arrey indefinida se da um console
+a arrey indefinida deja de ser e pasa a ser null
+e se le adiciona a palavra "b espaso ."
+arrey e os nomeros se da un console y array que que cantidade de numero tene 
+se declara uma variavei array I que se le da o valor de 0 
+da un colose y dize que array tiene o valor de d
+e se soma o array {1}
+array se soma I + 1 =19 
+e se deze que array e "e" 
+se declara um array con o nome de valor y deze que e igual a array I+6 
+da um cosole e deze que e f e o valor de isso*/  
+
+/*
+con os comando dados em o codigo o console exibiria uma frase em guardas con as
+letras "A" apenas por "1" e o numero de caracteres da frase e seria 23
+const frase = prompt("digite uma frase")
+console.log(frase.toUpperCase().replaseAll("A", "I"), frase.length)*/
+/*
+const nomeUsuario = prompt("digite seu nome")
+const emailUsuario = prompt("digite seu email")
+
+const frase = "o email" + emailUsuario + "foi cadastrado con sucesso" + "seja bem-vinda(o)" + nomeUsuario*/
+
+
+/*
+const comidasPreferidas = ["pizza", "hamburguer", "macarrao", "pastel de queijo", "acai"]
+console.log(comidasPreferidas)
+console.log("comidas preferidas:");
+
+console.log("essas sao as minhas comidas preferidas")
+console.log(comidasPreferidas ,1,
+    2,
+    3,
+    4)
+
+let novaComida = prompt("digite a sua comida preferida?")
+comidasPreferidas[1] = novaComida
+
+console.log("nova lista:", comidasPreferidas)
+*/
+/*
+let listaDeTarefas 
+console.log(prompt("digite uma tarefa") , listaDeTarefas)
+console.log(prompt("digite outra tarafa"), listaDeTarefas)
+console.log(prompt("digite outra tarefa"), listaDeTarefas)
+listaDeTarefas.push['arrumar as camas']
+listaDeTarefas.push('Lavar a louça')
+listaDeTarefas.push('Retirar o lixo')
+console.log(listaDeTarefas)
+prompt
+*/
+
+
+
+
+

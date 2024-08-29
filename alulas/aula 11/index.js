689e6b3477ca90d0b5b5323bf6ebb76b290c6800
ingrib30 <ingridbernardes@live.com>
2024-08-29 17:17:15 -0300

    aulas

---
+++ alulas/aula 11/index.js
@@ -0,0 +1,74 @@
+//exercicios de fixacao
+/*
+a linha acima solicita ao usuario que digite um numero armazene o valor digitado na variavel em 'respostaDoUsuario', 
+essa linha converte o valor da string armazenada em 'respostaDoUsuario' em numero e armazenada na variavel numero,
+ocodico utiliza a operacao do modulo '%' para verificar se o resto da divisao do numero por 2 e igual a 0
+o seu resto da ,se o numero for par 'o resto da divisao por 2 e igual a 0' o codigo imprima passou o teste sem console,
+o caso o numero nao seja par o codigo executa o bloco 'else' , se o numero no foi par o codigo imprime nao passou no teste 
+sem console
+a)o codigo realiza um teste de paridade verificando se o numero digitado pelo usuario ,
+b)o codigo imprime passou no teste apenas para numeros pares
+c)o codigo imprime nao passou no teste apenas para numeros impares 
+*/
+/*a)o codigo serve para criar um sistema de preco para difetentes tipos de frutas utilizando uma estrutura 'switch'
+que verifica o prego da variavel fruta e define o preco correspondentes 
+b)a mensagem impressa no console sera 'preco da fruta maca e R$2,25'
+c)sem o break a execusao do codigo cai para o default o que faz com o codigo execute conforme a as intrucoes do default
+sobrescrevendo o preco da pera com o valor padrao do default
+*//*
+A)a primeira linha e par asolicitar ao usuario que digite um numero con prompt e a funcao number estao converte a entrada 
+em um numero
+B)si o usuario digitar 10 a mensagem 'esse numero paso no teste ' sera impresaa no terminal seguida de 'essa mensagem e secreta',
+se o usuario digitar -10 nao havera nenhuma mensagem impressa no terminal ja que a condicao nao sera feita e o bloco de 
+codigo dentro do if nao sera executado
+C)nao havera erro no console , isso porque a variavel 'mensagem' e definida dentro do bloco if e essa variavel so sera criada
+se a condocao do if for verdadeira ou seja se o numero digitado for maior que 0 
+*/
+/*
+const idadeUsuario = Number(prompt('qual e sua idade?'))
+if(idadeUsuario > 17){
+    console.log('voce pode dirigir')
+}else{
+    console.log('voce nao pode dirigir')
+} */
+/*
+let turno = prompt('digite o turno do aluno (m para matutino, v para vespertino, n para noturno')
+if(turno === "m"){
+    console.log('bom dia')
+}else if(turno === "v"){
+    console.log('boa tarde')
+}else if (turno === "n"){
+    console.log('boa noite')
+}else {
+    console.log('turno invalido')
+}*/
+/*
+let turno = prompt('digite o turno do aluno (m para matutino, v para vespertino, n para noturno')
+switch(turno){
+    case 'm':
+    console.log('bom dia')
+    break
+     case 'v':
+     console.log('boa tarde')
+     break
+     case 'n':
+     console.log("boa noite")
+     default :
+     console.log('turno invalido')      
+}*/
+/*
+let generoFilme = prompt('qual o genero do filme que voce quere assistir')
+let precoIngreso = Number(prompt('qual e o preco do ingresso'))
+ if(generoFilme.toLowerCase()=== 'fantasia'&& precoIngreso <=15){
+    console.log('bom filme')
+}else{
+    console.log('escolha outro filme')
+} *//*
+const generoFilme = prompt('qual e o genero do filme que voce quere assitir')
+const precoIngreso = prompt('qual e o preco do ingresso')
+const lunchinho = prompt('qual e o lunchinho que vai comprar')
+if(generoFilme.toLowerCase()=== 'fantasia'&& precoIngreso <=15){
+    console.log('bom filme')
+}else if (lunchinho =inpu){}
+*/
+

d117277ebe535e95a3842eccd37f719765f6305a
ingrib30 <ingridbernardes@live.com>
2024-09-02 17:18:37 -0300

    aulas

---
+++ aulas/aula 12/index.js
@@ -0,0 +1,71 @@
+//exemplo
+/*let i = 0
+
+while(1<10){
+    console.log('o numero e', i)
+    i ++
+}*//*
+let estomago = 0
+while(estomago<100){
+    console.log('quero comer mais coxinhas',estomago)
+estomago = estomago+10
+}*//*
+let soma = 0
+let numero =10
+while(numero !== 0){
+    numero = Number(prompt('digite o numero'))
+    soma += numero
+    console.log('o numero digitado e:', numero)
+}
+console.log('o valor total foi ', soma)*/
+/*
+let c = 1
+do{
+    console.log('o valor de contador e:', c)
+    c++
+}while('')
+*//*
+let numero =[14,57,89,15,23]
+for(let i = 0 ;i>5;++ )
+    console.log(numero)*/
+/*let numero = [17,15,38,14,12,13]
+let numeroMaior = numero[0]
+for (let i  = 0 ;i<6; i++ ){
+    if(numeroMaior<numero[i]){
+        numeroMaior=numero[i]
+    }
+}
+console.log('o nmair numero e:', numeroMaior)*/
+/*/
+let numero = [17,15,38,14,12,13]
+let soma = 0
+for (let i  = 0 ;i<6; i++ ){
+   if(numero[i]%2 === 0){
+    soma += numero[i]
+   }
+}
+console.log('a soma dos numeros pares e:', soma)*/
+
+/*
+const numeros = [14,67,89,15,23]
+for(let numero of numeros){
+    console.log(numero)
+}*/
+/*
+let frase 
+let palavras = ['oi', 'sumido','tudo', 'bem?', 'saudade']
+for(let palavra of palavras){
+    frase += palavra+"  "
+    console.log(frase)
+}
+console.log(frase)*/
+/*
+let numeros= [11,15,18,14,12,13]
+let soma = 0
+for(let numero of numeros){
+if(numero%2 === 0){
+    soma += numero
+}
+}console.log('a soma dos numeros pares e:', soma)
+    *//*
+   let palavrasMaisLongas = ["numeros",'espaços','linguagem','legal']*/

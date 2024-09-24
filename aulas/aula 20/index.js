807bbcf966ef451167f9b90d6484deae96a5446f
ingrib30 <ingridbernardes@live.com>
2024-09-24 15:53:34 -0300

    aula

---
+++ aulas/aula 20/index.js
@@ -0,0 +1,33 @@
+let matriz= [
+    [1,2,3],
+    [4,5,6]
+]
+
+console.log(matriz[1][2])
+
+
+let matriz2=[
+    [10,20,30],
+    [40,50,60],
+    [70,80,90]
+]
+console.log(matriz2[0][1])//retorna 20
+console.log(matriz2[1][2])//retorna 60
+console.log(matriz2[1][0])//retorna 40
+
+let matriz3=[]
+matriz3[0]=[1,2,3]
+matriz3[1]=[4,5,6]
+matriz3[2]=[7,8,9]
+
+
+let velha=[
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
+velha[1][2]=1
+console.log(velha)
+
+
+

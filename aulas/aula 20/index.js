43d6a58c26db59bb057ed0a6b6a71dfc4f1aeea4
ingrib30 <ingridbernardes@live.com>
2024-09-30 14:14:25 -0300

    aula

--- aulas/aula 20/index.js
+++ aulas/aula 20/index.js
@@ -1,4 +1,4 @@
-let matriz= [
+/*let matriz= [
     [1,2,3],
     [4,5,6]
 ]
@@ -29,5 +29,79 @@ let velha=[
 velha[1][2]=1
 console.log(velha)
 
+/*
+let n =[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+for(let i=0; i<n.length;i++){
+for(let j=0; j<n[i].length;j++){
+    console.log(n[i][j])
+}
+}
+*/
+/*
+let n =[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+for(let i=0; i<n.length;i++){
+for(let j=0; j<n[i].length;j++){
+    soma +=n[i][j]
+}
+}
 
+ console.log(soma)
+*/
+/*
+let cafe = [
+    [1, 2, 3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13, 14, 15,16]
+]
+function soma(n) {
+    let valor = 0
+    for (let i = 0; i < n.length; i++) {
+        for (let j = 0; j < n[i].length; j++) {
+            valor += n[i][j]
+        }
+    }
+    console.log(valor)
+}
+soma(cafe)
 
+let tabuleiro=[
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0]
+
+]
+function posisonarNavil(){
+    tabuleiro[9][8]=1
+    tabuleiro[5][9]=1
+    tabuleiro[4][7]=1
+    tabuleiro[1][6]=1
+
+}
+let soma =0
+for (let i=0; i<tabuleiro.length; i++){
+    for(let j=0; j<tabuleiro[i].length;j++)
+        quantidade+=tabuleiro[i][j]
+}
+let escolha=Number(prompt("escolha linha"))
+let segundaEscolha=Number(prompt("escolha coluna"))
+
+function verificar(linha,coluna){
+    
+}
+*/
---
+++ aulas/aula 21/index.js
@@ -0,0 +1,11 @@
+//exercicio 1 interpretacao de codigo 
+/* o codigo inprimira o numero 16 no console  
+o codigo assede  a linha 2 
+let matriz = [
+    [2,4,6],
+    [8,10,12],
+    [14,16,18]
+]
+console.log(matriz[2][1]) 
+*/
+

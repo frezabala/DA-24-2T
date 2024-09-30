30c8108a45636bcbc8c67db2d4aab6f322615b81
ingrib30 <ingridbernardes@live.com>
2024-09-30 17:07:56 -0300

    aula

--- aulas/aula 21/index.js
+++ aulas/aula 21/index.js
@@ -1,6 +1,6 @@
 //exercicio 1 interpretacao de codigo 
 /* o codigo inprimira o numero 16 no console  
-o codigo assede  a linha 2 
+o codigo assede  a linha 2 e coluna 1
 let matriz = [
     [2,4,6],
     [8,10,12],
@@ -8,4 +8,93 @@ let matriz = [
 ]
 console.log(matriz[2][1]) 
 */
+
+//exercicio 2 
+/* o codigo imprimirao numeros '2, 4 e 6 
+let matriz=[
+    [1,2],
+    [3,4],
+    [5,6,7]
+];
+for (let i =0; i< matriz.length; i++){
+    for(let j =0; j< matriz[i].length; j++){
+        if(matriz[i][j] % 2 === 0){
+            console.log(matriz[i][j]);
+        }
+    }
+}*/
+ 
+
+//execicio 3
+/* o valor final da variavel 'soma' sera a soma de todas os numeros da matriz 
+portanto o valor final da 'soma' da variavel e 225 
+let matriz = [
+    [5, 10, 15],
+    [20, 25, 30],
+    [35, 40, 45]
+  ];
+  
+  let soma = 0;
+  for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+      soma += matriz[i][j]; 
+    }
+  }
+  
+  console.log(soma); */
+
+
+
+  // exercicio 1 escrita de codigo 
+  /*let matriz =[
+    [0,1,0,2,1],
+    [2,1,0,1,0],
+    [2,2,1,0,1],
+    [0,0,1,2,1],
+    [0,1,2,0,1]
+]*/
+
+//exercicio 2
+/*
+const matriz = [
+    [10, 8, 12, 9], 
+    [7, 15, 9, 11], 
+    [12, 9, 10, 13]
+  ];
+  
+  for (let i = 0; i < matriz.length; i++) {
+    let pontuacaoTotal = 0;
+    for (let j = 0; j < matriz[i].length; j++) {
+      pontuacaoTotal += matriz[i][j];
+    }
+    console.log('Pontuação total do Jogador' );
+  }
+*/
+//exercicio  3
+/*function inverterLinhasMatriz(matriz) {
+    for (let i = 0; i < matriz.length; i++) {
+      matriz[i].reverse(); 
+    }
+    return matriz;
+  }
+  
+
+  let matriz = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+  ];
+  
+  let matrizInvertida = inverterLinhasMatriz(matriz);
+  
+  
+  console.log(matrizInvertida);
+*/
+
+
+
+
+//defasio
+
 
